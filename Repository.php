905ba4cbd088b60bfb36a52ed28fee71a8a5fd53
Matheus Minski <minskisantos@hotmail.com>
2024-11-06 23:21:58 -0300

    feat(burguer-store): first commit

---
+++ Repository.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<?php
+
+$mysqli = new mysqli(
+    "127.0.0.1",
+    "root",
+    "strong_password",
+    "burguer-store",
+    3307
+);
+
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Burguer Store</title>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+
+<body>
+    <div id="app">
+        <div id="tableTitle" class="flex-center">
+            <h1>Estoque de produtos</h1>
+        </div>
+        <div id="tableContent" class="flex-center">
+            <table id="productsTable">
+                <th>Id</th>
+                <th>Nome</th>
+                <th>Quantidade</th>
+                <th>Ações</th>
+                <tr>
+                    <td>123</td>
+                    <td>Pão</td>
+                    <td>15</td>
+                    <td><button class="btn">editar</button></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
